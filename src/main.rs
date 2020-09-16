8fa4f88dabdccb45998d7ab982f568eedf38cbe2
taronaldinho <kotaro.yamashita@gmail.com>
2020-09-17 05:56:14 +0900

    clapの機能テスト_途中

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("rpwgen")
+    .version("0.0.1")
+    .author("Kotaro Yamashita")
+    .about("Generate Passwords")
+    .arg(
+        Arg::with_name("length_of_password")
+        .value_name("LENGTH_OF_PASSWORD")
+        .short('l')
+        .long("length")
+        .index(1)
+        .required(false),
+    )
+    .get_matches();
+
+    match matches.value_of("length_of_password") {
+        Some(length) => println!("length_of_password specified {}.", length),
+        None => println!("length_of_password is not specified.")
+    }
 }
